8ff301a39d343950497cb2f5332564dbd2a77920
marioq <mario300@gmail.com>
2022-08-19 16:05:27 -0300

    chore: header component update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
 import styles from './styles.module.scss'
+import { useAuth } from '../../context/SurveyContext'
 
 const Header: NextPage = () => {
+  const { userAccount } = useAuth()
+
   return (
     <div className={styles.header}>
       <h1>Survey App</h1>
+      <p>{ userAccount }</p>
     </div>
   )
 }
--- src/pages/survey/index.tsx
+++ src/pages/survey/index.tsx
@@ -1,15 +1,27 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { Content } from 'antd/lib/layout/layout'
 import styles from './styles.module.scss'
-import Header from '../../components/Header';
-import Dashboard from '../../components/Dashboard';
-
+import Header from '../../components/Header'
+import Dashboard from '../../components/Dashboard'
+import { useAuth } from '../../context/SurveyContext'
 
 const Survey: NextPage = () => {  
+
+  const { userAccount } = useAuth()
+  const router = useRouter()
+
+  useEffect(() => { 
+    !userAccount ? router.push('/login') : null
+  }, [userAccount])
+
   return (
     <Content>
-      <Header />
-      <Dashboard />
+      <div className={styles.container}>
+        <Header />
+        <Dashboard />
+      </div>
     </Content>
   )
 }
